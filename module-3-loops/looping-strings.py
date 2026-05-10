86faf9032747103f33b76e15f68750a4c9d829ff
Marco Hope <marcomateohope@gmail.com>
2026-05-09 20:35:23 -0400

    update idk

---
+++ module-3-loops/looping-strings.py
@@ -0,0 +1,19 @@
+# greeting = 'Hello'
+
+# for char in greeting:
+# 	print(char)
+	
+# for i in range(len(greeting)):
+# 	print(i)
+	
+# greeting = 'Hello'
+# index = 0
+# while index < len(greeting):
+# 	print(greeting[index])
+# 	index += 1
+	
+greeting = 'Hello'
+index = 0
+while index < len(greeting):
+    print(greeting[index:index+1])
+index += 1
---
+++ module-3-loops/practice3.py
@@ -0,0 +1,13 @@
+input = "Four score and seven years ago"
+
+# for c in input:
+#   if c.lower() in ['a', 'e', 'i', 'o', 'u']:
+#     print(c)
+
+# print([c for c in input if c.lower() in ['a', 'e', 'i', 'o', 'u']])
+
+# print(input.count("aeiou"))
+
+for c in range(len(input)):
+  if c in ['a', 'e', 'i', 'o', 'u']:
+    print(c)
